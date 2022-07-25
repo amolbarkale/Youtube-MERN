d3a09d6b17b64ecd0adbe53d86be33a623cd45bd
amolbarkale <amol@gameopedia.com>
2022-07-25 11:26:07 +0530

    code upated

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -147,7 +147,7 @@ const Menu = ({darkMode, setDarkMode}) => {
         </Item>
         <Item onClick={()=>setDarkMode(!darkMode)}>
           <SettingsBrightnessOutlinedIcon />
-          Mode
+          {darkMode? "Light": "Dark"} Mode
         </Item>
       </Wrapper>
     </Container>
